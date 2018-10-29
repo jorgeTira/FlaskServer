5806b44b4d6ecc0ef087554a08c6b020aeb9b4c2
jorgeTira <jorge.tirado@alumnos.udg.mx>
2018-10-29 14:36:14 -0600

    origin

--- servidor.py
+++ servidor.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import mysql.connector
-from usuario import Usuario
+from Usuario import Usuario
 
 conexion = mysql.connector.connect(user = "luis",
                            password = "12345",
@@ -16,7 +16,7 @@ def login():
     usuario = request.args.get('usuario')
     passw = request.args.get('password')
     userDB = Usuario(conexion, cursor)
-    print(userDB.login(usuario, password))
+    print(userDB.login(usuario, passw))
     return usuario
 
 if __name__=='__main__':
